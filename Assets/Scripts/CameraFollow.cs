f203c0713f3c015e3af23d46ea903ee50a75964a
Liam Chessell <chessell1985@gmail.com>
2016-02-02 20:32:52 +0000

    first commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float xMargin = 1f;
+	public float xSmooth = 1f;
+	public float xMin = -13.5f;
+	public float xMax = 13.5f;
+
+	public GameObject ground;
+	public GameObject player;
+
+	private Transform playerTransform;
+
+	// Use this for initialization
+	void Awake () {
+	
+		playerTransform = player.transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		TrackPlayer ();
+
+	}
+
+	bool CheckXMargin () {
+		return Mathf.Abs(transform.position.x - playerTransform.position.x) > xMargin;
+	}
+
+	void TrackPlayer () {
+
+		float targetX = transform.position.x;
+
+		// If the player has moved beyond the x margin...
+		if (CheckXMargin ()) {
+
+			targetX = Mathf.Lerp(transform.position.x, playerTransform.position.x, xSmooth * Time.deltaTime);
+
+		}
+
+		// The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
+		targetX = Mathf.Clamp(targetX, xMin, xMax);
+
+		// Set the camera's position to the target position with the same z component.
+		transform.position = new Vector3(targetX, transform.position.y, transform.position.z);
+
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public bool facingRight = true;
+
+	public float moveForce = 365f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Awake () {
+
+		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		float h = Input.GetAxis ("Horizontal");
+
+		if (h == 0) {
+		
+			rb2d.velocity = new Vector2 (0f, rb2d.velocity.y);
+		
+		}
+
+		if (h * rb2d.velocity.x < maxSpeed) {
+
+			rb2d.AddForce (Vector2.right * h * moveForce);
+
+		}
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed) {
+
+			rb2d.velocity = new Vector2 (Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+		}
+
+	}
+}
